e4a6b3dec71f03cd6ee04566635827f6c2011e5a
Corwin96 <corwinvandalen@gmail.com>
2022-06-22 13:16:17 +0200

    Subgraph eth nft

---
+++ titan-nft-ethereum/src/mapping.ts
@@ -0,0 +1,34 @@
+import { Transfer as TransferEvent, TitanNFTContract as NFTContract } from "../generated/TitanNFTContract/TitanNFTContract";
+import { Nft, ContractInfo } from "../generated/schema";
+
+export function handleTransfer(event: TransferEvent): void {
+
+    // NFT Owner
+  let nft = Nft.load(event.transaction.hash.toHex());
+
+  if(!nft) {
+    nft = new Nft(event.transaction.hash.toHex());
+  }
+
+  nft.to = event.params.to.toHex();
+  nft.from = event.params.from.toHex();
+  nft.owner = event.params.to.toHex();
+  nft.tokenID = event.params.tokenId;
+  
+  const tokenContract = NFTContract.bind(event.address);
+  nft.contentURI = tokenContract.tokenURI(event.params.tokenId);
+
+  nft.save();
+
+  // ContractInfo 
+  let contractInfo = ContractInfo.load(event.transaction.hash.toHex());
+  if(!contractInfo) {
+    contractInfo = new ContractInfo(event.transaction.hash.toHex());
+  }
+
+  contractInfo.totalSupply = tokenContract._maxSupply();
+  contractInfo.totalMinted = tokenContract.totalSupply();
+
+  contractInfo.save();
+
+}
